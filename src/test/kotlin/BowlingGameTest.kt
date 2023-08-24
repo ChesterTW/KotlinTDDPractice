58c810e1ec5fbec8575502ff779f76607377c434
TaroCheng <taro_cheng@appdet.com>
2023-08-24 11:45:38 +0800

    [Clean] Refactor Test

--- src/test/kotlin/BowlingGameTest.kt
+++ src/test/kotlin/BowlingGameTest.kt
@@ -5,18 +5,24 @@ import org.junit.jupiter.api.Test
 
 class BowlingGameTest {
 
+    private val bowlingGame = BowlingGame()
 
     @Test
     fun all_zero(){
-        val bowlingGame = BowlingGame()
-        for (i in 1..20){
-            bowlingGame.roll(0)
-        }
+        rolls(20, 0)
         val score = bowlingGame.score()
-        Assertions.assertEquals(0,score)
+        scoreShouldBe(score)
     }
 
+    private fun scoreShouldBe(score: Any) {
+        Assertions.assertEquals(score, bowlingGame.score())
+    }
 
+    private fun rolls(rollNumber: Int, pins: Int) {
+        for (i in 1..rollNumber) {
+            bowlingGame.roll(pins)
+        }
+    }
 
 
 }
